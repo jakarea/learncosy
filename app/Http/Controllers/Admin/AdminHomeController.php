3dfe190390353ecee887434784da5806d1fb75af
Jakarea Parvez জিহাদ <jakareaparvez@gmail.com>
2023-06-08 23:51:35 +0600

    admin dashboard

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -13,7 +13,19 @@ class AdminHomeController extends Controller
 {
     // dashboard
     public function dashboard(){
-        $courses = Course::orderBy('id', 'desc')->get();
-        return view('e-learning/course/admin/dashboard',compact('courses')); 
+        $courses = Course::count();
+        $userCounts = User::select('user_role', \DB::raw('count(*) as total'))
+            ->groupBy('user_role')
+            ->pluck('total', 'user_role');
+
+            foreach ($userCounts as $role => $count) {
+                if($role == 'student')
+                    $students = $count;
+
+                if($role == 'instructor')
+                    $instructors = $count;
+            }
+
+        return view('e-learning/course/admin/dashboard',compact('courses','students','instructors','courses')); 
     }
 }
