541c62c28981633e77485c06ce426d62b3004a37
Md Arif <arifypp@gmail.com>
2023-06-09 14:01:33 +0600

    stripe validation

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Stripe\Exception\AuthenticationException;
 
 class SettingsController extends Controller
 {
@@ -18,25 +19,41 @@ class SettingsController extends Controller
      // Update stripe data
      public function stripeUpdate(Request $request)
      {
-        $connet = '';
          $user = auth()->user();
-         // stripe_secret_key and stripe_public_key is an valid stripe can access
-         if (!$request->stripe_secret_key || !$request->stripe_public_key) {
-             return back()->with('error', 'Please enter valid stripe keys');
-         }
-
-         $user->stripe_secret_key = $request->stripe_secret_key ? $request->stripe_secret_key : $user->stripe_secret_key;
-         $user->stripe_public_key = $request->stripe_public_key ? $request->stripe_public_key : $user->stripe_public_key;
+     
+         // Define custom error messages
+         $messages = [
+             'stripe_secret_key.required' => 'Please enter a valid Stripe secret key.',
+             'stripe_public_key.required' => 'Please enter a valid Stripe public key.',
+         ];
+     
+         // Validate the Stripe keys using the Stripe package's validation feature
+         $request->validate([
+             'stripe_secret_key' => [
+                 'required',
+                 function ($attribute, $value, $fail) {
+                     try {
+                         \Stripe\Stripe::setApiKey($value);
+                         \Stripe\Account::retrieve();
+                     } catch (AuthenticationException $e) {
+                         $fail('Please enter a valid Stripe secret key.');
+                        $messages['stripe_secret_key.required'] = 'Please enter a valid Stripe secret key.';
+                     }
+                 },
+             ],
+             'stripe_public_key' => 'required',
+         ], $messages);
+     
+         $user->stripe_secret_key = $request->stripe_secret_key;
+         $user->stripe_public_key = $request->stripe_public_key;
          $user->save();
-
-         // check stripe connected or not
-        if( !$user->stripe_secret_key || !$user->stripe_public_key ){
-            $connet = 'not';
-        }else{
-            $connet = 'yes';
-        }
+     
+         // Check if both keys are present
+         $connected = ($user->stripe_secret_key && $user->stripe_public_key) ? 'yes' : 'not';
+     
          return back()->with('success', 'Stripe data updated successfully');
      }
+          
 
      // vimeo settings
      public function vimeoIndex()
