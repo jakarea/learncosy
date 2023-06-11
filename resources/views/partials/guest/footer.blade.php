77818d8dc0da05c0a66519a9826145cbea1ddc90
Akram Hossain <nayanbd17@gmail.com>
2023-06-11 20:44:20 +0600

    landing page details page design dynamic

---
+++ resources/views/partials/guest/footer.blade.php
@@ -0,0 +1,47 @@
+{{-- footer section @s --}}
+<footer class="footer-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-6">
+                <div class="ftr-links">
+                    <h5>Menu</h5>
+
+                    <ul class="custom_nav">
+                        <li><a href="{{url('/students/dashboard')}}">Home</a></li>
+                        <li><a href="#course_sec">Course</a></li>
+                        <li><a href="#b_course_sec">Bundle Course</a></li>
+                        <li><a href="#feedback_sec">Fededback</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-6">
+                <div class="ftr-links">
+                    <h5>E-Learning</h5>
+
+                    <ul>
+                        <li><a href="#">Courses</a></li>
+                        <li><a href="#">Bundle Course</a></li>
+                        <li><a href="#">Modules</a></li>
+                        <li><a href="#">Lessons</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-6">
+                <div class="ftr-links">
+                    <h5>Support</h5>
+
+                    <ul>
+                        <li><a href="#">TOS</a></li> 
+                        <li><a href="#">Faq</a></li> 
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="ftr-copy">
+                    <p>All Right Reserved &copy; Learcosy 2023</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+{{-- footer section @e --}}
---
+++ resources/views/partials/guest/get-start.blade.php
@@ -0,0 +1,14 @@
+{{-- get start @s --}}
+<section class="get-start">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <div class="get-start-box">
+                    <h5>Get the best Instructor and best course <br> over the world!</h5>
+                    <a href="{{url('/students/dashboard/enrolled')}}">Get enroll now!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+{{-- get start @e --}}
