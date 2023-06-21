f24ba3f79721e77b155678680377b8101ae23421
Md Monirul Islam <mmislam027@gmail.com>
2023-06-22 00:09:57 +0600

    earning chart update with dynamic data

--- resources/views/dashboard/instructor/dashboard.blade.php
+++ resources/views/dashboard/instructor/dashboard.blade.php
@@ -83,7 +83,7 @@
                     <div class="statics-head">
                         <h5>Earnings</h5>
                     </div>
-                    <div id="chart-timeline"></div>
+                    <div id="earningChart"></div>
                 </div>
                 <!-- project statistic @e -->
             </div>
@@ -338,7 +338,103 @@
         var chart = new ApexCharts(document.querySelector("#lineChart"), options);
         chart.render();
     </script>
+    <script>
+        // chart for Earnings
+        const earnings = @json($earningByDates);
+        const earningsKeys = Object.keys(earnings)
+        const minDateForThisChart = earningsKeys.reduce((acc, current) => current < acc ? current : acc, earningsKeys[0])
+        const dataForSeries = Object.entries(earnings).map(([key, value]) => {
+            return [new Date(key).getTime(), value]
+        })
 
+        var options = {
+            series: [{
+                data: dataForSeries,
+            }, ],
+            chart: {
+                id: "area-datetime",
+                type: "area",
+                height: 350,
+                zoom: {
+                    autoScaleYaxis: true,
+                },
+            },
+            annotations: {
+                yaxis: [{
+                    y: 30,
+                    borderColor: "#999",
+                    label: {
+                        show: true,
+                        text: "Support",
+                        style: {
+                            color: "#fff",
+                            background: "#00E396",
+                        },
+                    },
+                }, ],
+                xaxis: [{
+                    x: new Date(Math.min(...(Object.keys(earnings)))).getTime(),
+                    borderColor: "#999",
+                    yAxisIndex: 0,
+                    label: {
+                        show: true,
+                        text: "Rally",
+                        style: {
+                            color: "#fff",
+                            background: "#775DD0",
+                        },
+                    },
+                }, ],
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            markers: {
+                size: 0,
+                style: "hollow",
+            },
+            grid: {
+                show: true,
+                borderColor: "#D7D7D7",
+                strokeDashArray: 0,
+                position: "back",
+                xaxis: {
+                    lines: {
+                        show: false,
+                    },
+                },
+                yaxis: {
+                    lines: {
+                        show: true,
+                        opacity: 0.1,
+                    },
+                },
+            },
+            xaxis: {
+                type: "datetime",
+                min: minDateForThisChart,
+                tickAmount: 6,
+            },
+            tooltip: {
+                x: {
+                    format: "dd MMM yyyy",
+                },
+            },
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    opacityFrom: 0.7,
+                    opacityTo: 0.9,
+                    stops: [0, 100],
+                },
+            },
+        };
+
+
+        var chart = new ApexCharts(document.querySelector("#earningChart"), options);
+        chart.render();
+    </script>
     <script>
         function updateClock() {
             var currentTime = new Date();
