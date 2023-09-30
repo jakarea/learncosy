b504547c816fff29ad4f6ff9eed095c6ca4b9e9b
asif <oronno.mbstu.cse@gmail.com>
2023-09-30 17:14:21 +0600

    update instructor profile image

--- app/Http/Controllers/ProfileManagementController.php
+++ app/Http/Controllers/ProfileManagementController.php
@@ -13,24 +13,26 @@ use App\Mail\ProfileUpdated;
 use App\Mail\PasswordChanged;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Hash;
+use Intervention\Image\Facades\Image;
+
 class ProfileManagementController extends Controller
 {
 
     // profile show
     public function show()
-    {  
+    {
         $id = Auth::user()->id;
-        $user = User::find($id); 
+        $user = User::find($id);
         $courses = Course::where('user_id', $id)->get();
         $experiences = Experience::where('user_id', Auth::user()->id)->orderBy('id','desc')->get();
-        return view('profile/instructor/profile',compact('user','courses','experiences')); 
+        return view('profile/instructor/profile',compact('user','courses','experiences'));
     }
 
     // profile edit
     public function edit(Request $request)
-    {   
+    {
         $experience_id = $request->query('id');
-        $userId = Auth()->user()->id;  
+        $userId = Auth()->user()->id;
         $user = User::find($userId);
         $editExp = '';
         if($experience_id){
@@ -39,22 +41,22 @@ class ProfileManagementController extends Controller
         // return $editExp;
         $experiences = Experience::where('user_id', Auth::user()->id)->orderBy('id','desc')->get();
         // dd($user);
-        return view('profile/instructor/edit',compact('user','experiences','editExp'));  
+        return view('profile/instructor/edit',compact('user','experiences','editExp'));
     }
 
     public function update(Request $request)
     {
         // return $request->all();
 
-        $userId = Auth()->user()->id;  
+        $userId = Auth()->user()->id;
 
         $this->validate($request, [
             'name' => 'required|string',
-            'short_bio' => 'required|string', 
-            'phone' => 'required|string', 
+            'short_bio' => 'required|string',
+            'phone' => 'required|string',
             'avatar' => 'image|mimes:jpeg,png,jpg,gif,svg,webp|max:5000',
         ]);
-       
+
         $user = User::where('id', $userId)->first();
         $user->name = $request->name;
 
@@ -62,35 +64,44 @@ class ProfileManagementController extends Controller
             $user->subdoamin = $user->subdoamin;
         }else{
             $user->subdoamin =  Str::slug($request->subdoamin);
-        } 
+        }
         $user->short_bio = $request->short_bio;
         $user->social_links = implode(",",$request->social_links);
         $user->phone = $request->phone;
         $user->description = $request->description;
         $user->recivingMessage = $request->recivingMessage;
-        $user->email = $user->email; 
+        $user->email = $user->email;
         if ($request->password) {
             $user->password = Hash::make($request->password);
         }else{
             $user->password = $user->password;
-        }  
+        }
 
-        if ($request->hasFile('avatar')) { 
-            // Delete old file
-            if ($user->avatar) {
-               $oldFile = public_path($user->avatar);
-               if (file_exists($oldFile)) {
-                   unlink($oldFile);
-               }
-           } 
-           $slugg = Str::slug($request->name);
-           $image = $request->file('avatar');
-           $name = $slugg.'-'.uniqid().'.'.$image->getClientOriginalExtension();
-           $destinationPath = public_path('/assets/images/users');
-           $image->move($destinationPath, $name);
-           $user->avatar = $name; 
-       }
-
+    //     if ($request->hasFile('avatar')) {
+    //         // Delete old file
+    //         if ($user->avatar) {
+    //            $oldFile = public_path($user->avatar);
+    //            if (file_exists($oldFile)) {
+    //                unlink($oldFile);
+    //            }
+    //        }
+    //        $slugg = Str::slug($request->name);
+    //        $image = $request->file('avatar');
+    //        $name = $slugg.'-'.uniqid().'.'.$image->getClientOriginalExtension();
+    //        $destinationPath = public_path('/assets/images/users');
+    //        $image->move($destinationPath, $name);
+    //        $user->avatar = $name;
+    //    }
+       $image_path = '';
+       if ($request->hasFile('avatar')) {
+            $file = $request->file('avatar');
+            $image = Image::make($file);
+            $slugg = Str::slug($request->name);
+            $uniqueFileName = $slugg . '-' . uniqid() . '.png';
+            $image->save(public_path('assets/images/users/') . $uniqueFileName);
+            $image_path = 'assets/images/users/' . $uniqueFileName;
+        }
+        $user->avatar = $image_path;
         $user->save();
 
         // Send email
@@ -101,11 +112,11 @@ class ProfileManagementController extends Controller
 
     // password update
     public function passwordUpdate()
-    {    
-        $userId = Auth()->user()->id;  
+    {
+        $userId = Auth()->user()->id;
         $user = User::find($userId);
 
-        return view('profile/instructor/password-change',compact('user'));  
+        return view('profile/instructor/password-change',compact('user'));
     }
 
     public function postChangePassword(Request $request)
@@ -117,7 +128,7 @@ class ProfileManagementController extends Controller
             'password' => 'required|confirmed|min:6|string',
         ]);
 
-        $userId = Auth()->user()->id; 
+        $userId = Auth()->user()->id;
         $user = User::where('id', $userId)->first();
         $user->password = Hash::make($request->password);
         $user->save();
