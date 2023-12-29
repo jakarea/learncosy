b0bf195118aab84dd2edf3e24414897c579e518b
Akram Hossain <nayanbd17@gmail.com>
2023-12-29 12:44:26 +0600

    module

--- resources/views/e-learning/course/students/show.blade.php
+++ resources/views/e-learning/course/students/show.blade.php
@@ -240,13 +240,15 @@
                                         </div>
                                         <div id="collapse_{{ $module->id }}"
                                             class="accordion-collapse collapse
-                                {{ $currentLesson && $currentLesson->module_id == $module->id ? 'show' : '' }}"
+                                            {{ $currentLesson && $currentLesson->module_id == $module->id ? 'show' : '' }}"
                                             aria-labelledby="heading_{{ $module->id }}"
                                             data-bs-parent="#accordionExample">
                                             <div class="accordion-body p-0">
                                                 <ul class="lesson-wrap">
                                                     @foreach ($module->lessons as $lesson)
-                                                        @if (!empty($lesson->text) || !empty($lesson->audio) || !empty($lesson->video_link))
+                                                        {{-- @if (!empty($lesson->text) || !empty($lesson->audio) || !empty($lesson->video_link)) --}}
+                                                        @if ($lesson->status == 'published')
+
                                                             <li>
                                                                 @if (!isEnrolled($course->id))
                                                                     <a href="{{ route('students.checkout', ['slug' => $course->slug, 'subdomain' => config('app.subdomain')]) }}"
@@ -257,7 +259,7 @@
                                                                 @else
                                                                     <a href="{{ $lesson->video_link }}"
                                                                         class="video_list_play d-inline-block
-                                                {{ $currentLesson && $currentLesson->id == $lesson->id ? 'active' : '' }}"
+                                                                {{ $currentLesson && $currentLesson->id == $lesson->id ? 'active' : '' }}"
                                                                         data-video-id="{{ $lesson->id }}"
                                                                         data-lesson-id="{{ $lesson->id }}"
                                                                         data-course-id="{{ $course->id }}"
